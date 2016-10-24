f5151f2623ec0a92c48b51a66732702640755f80
joewez <joewez@outlook.com>
2016-10-24 10:04:24 -0500

    Imbedded Line and Circle and removed SPI class

--- ssd1306.py
+++ ssd1306.py
@@ -1,1 +1,184 @@
-# MicroPython SSD1306 OLED driver, I2C and SPI interfacesimport timeimport framebuf# register definitionsSET_CONTRAST        = const(0x81)SET_ENTIRE_ON       = const(0xa4)SET_NORM_INV        = const(0xa6)SET_DISP            = const(0xae)SET_MEM_ADDR        = const(0x20)SET_COL_ADDR        = const(0x21)SET_PAGE_ADDR       = const(0x22)SET_DISP_START_LINE = const(0x40)SET_SEG_REMAP       = const(0xa0)SET_MUX_RATIO       = const(0xa8)SET_COM_OUT_DIR     = const(0xc0)SET_DISP_OFFSET     = const(0xd3)SET_COM_PIN_CFG     = const(0xda)SET_DISP_CLK_DIV    = const(0xd5)SET_PRECHARGE       = const(0xd9)SET_VCOM_DESEL      = const(0xdb)SET_CHARGE_PUMP     = const(0x8d)class SSD1306:    def __init__(self, height, external_vcc):        self.width = 128        self.height = height        self.external_vcc = external_vcc        self.pages = self.height // 8        self.buffer = bytearray(self.pages * self.width)        self.framebuf = framebuf.FrameBuffer1(self.buffer, self.width, self.height)        self.poweron()        self.init_display()    def init_display(self):        for cmd in (            SET_DISP | 0x00, # off            # address setting            SET_MEM_ADDR, 0x00, # horizontal            # resolution and layout            SET_DISP_START_LINE | 0x00,            SET_SEG_REMAP | 0x01, # column addr 127 mapped to SEG0            SET_MUX_RATIO, self.height - 1,            SET_COM_OUT_DIR | 0x08, # scan from COM[N] to COM0            SET_DISP_OFFSET, 0x00,            SET_COM_PIN_CFG, 0x02 if self.height == 32 else 0x12,            # timing and driving scheme            SET_DISP_CLK_DIV, 0x80,            SET_PRECHARGE, 0x22 if self.external_vcc else 0xf1,            SET_VCOM_DESEL, 0x30, # 0.83*Vcc            # display            SET_CONTRAST, 0xff, # maximum            SET_ENTIRE_ON, # output follows RAM contents            SET_NORM_INV, # not inverted            # charge pump            SET_CHARGE_PUMP, 0x10 if self.external_vcc else 0x14,            SET_DISP | 0x01): # on            self.write_cmd(cmd)        self.fill(0)        self.show()    def poweroff(self):        self.write_cmd(SET_DISP | 0x00)    def contrast(self, contrast):        self.write_cmd(SET_CONTRAST)        self.write_cmd(contrast)    def invert(self, invert):        self.write_cmd(SET_NORM_INV | (invert & 1))    def show(self):        self.write_cmd(SET_COL_ADDR)        self.write_cmd(0)        self.write_cmd(self.width - 1)        self.write_cmd(SET_PAGE_ADDR)        self.write_cmd(0)        self.write_cmd(self.pages - 1)        self.write_data(self.buffer)    def fill(self, col):        self.framebuf.fill(col)    def pixel(self, x, y, col):        self.framebuf.pixel(x, y, col)    def scroll(self, dx, dy):        self.framebuf.scroll(dx, dy)    def text(self, string, x, y, col=1):        self.framebuf.text(string, x, y, col)class SSD1306_I2C(SSD1306):    def __init__(self, height, i2c, addr=0x3c, external_vcc=False):        self.i2c = i2c        self.addr = addr        self.temp = bytearray(2)        super().__init__(height, external_vcc)    def write_cmd(self, cmd):        self.temp[0] = 0x80 # Co=1, D/C#=0        self.temp[1] = cmd        self.i2c.writeto(self.addr, self.temp)    def write_data(self, buf):        self.temp[0] = self.addr << 1        self.temp[1] = 0x40 # Co=0, D/C#=1        self.i2c.start()        self.i2c.write(self.temp)        self.i2c.write(buf)        self.i2c.stop()    def poweron(self):        pass
+# MicroPython SSD1306 OLED driver, I2C and SPI interfaces
+
+import time
+import framebuf
+
+# register definitions
+SET_CONTRAST        = const(0x81)
+SET_ENTIRE_ON       = const(0xa4)
+SET_NORM_INV        = const(0xa6)
+SET_DISP            = const(0xae)
+SET_MEM_ADDR        = const(0x20)
+SET_COL_ADDR        = const(0x21)
+SET_PAGE_ADDR       = const(0x22)
+SET_DISP_START_LINE = const(0x40)
+SET_SEG_REMAP       = const(0xa0)
+SET_MUX_RATIO       = const(0xa8)
+SET_COM_OUT_DIR     = const(0xc0)
+SET_DISP_OFFSET     = const(0xd3)
+SET_COM_PIN_CFG     = const(0xda)
+SET_DISP_CLK_DIV    = const(0xd5)
+SET_PRECHARGE       = const(0xd9)
+SET_VCOM_DESEL      = const(0xdb)
+SET_CHARGE_PUMP     = const(0x8d)
+
+class SSD1306:
+    def __init__(self, height, external_vcc):
+        self.width = 128
+        self.height = height
+        self.external_vcc = external_vcc
+        self.pages = self.height // 8
+        self.buffer = bytearray(self.pages * self.width)
+        self.framebuf = framebuf.FrameBuffer1(self.buffer, self.width, self.height)
+        self.poweron()
+        self.init_display()
+
+    def init_display(self):
+        for cmd in (
+            SET_DISP | 0x00, # off
+            # address setting
+            SET_MEM_ADDR, 0x00, # horizontal
+            # resolution and layout
+            SET_DISP_START_LINE | 0x00,
+            SET_SEG_REMAP | 0x01, # column addr 127 mapped to SEG0
+            SET_MUX_RATIO, self.height - 1,
+            SET_COM_OUT_DIR | 0x08, # scan from COM[N] to COM0
+            SET_DISP_OFFSET, 0x00,
+            SET_COM_PIN_CFG, 0x02 if self.height == 32 else 0x12,
+            # timing and driving scheme
+            SET_DISP_CLK_DIV, 0x80,
+            SET_PRECHARGE, 0x22 if self.external_vcc else 0xf1,
+            SET_VCOM_DESEL, 0x30, # 0.83*Vcc
+            # display
+            SET_CONTRAST, 0xff, # maximum
+            SET_ENTIRE_ON, # output follows RAM contents
+            SET_NORM_INV, # not inverted
+            # charge pump
+            SET_CHARGE_PUMP, 0x10 if self.external_vcc else 0x14,
+            SET_DISP | 0x01): # on
+            self.write_cmd(cmd)
+        self.fill(0)
+        self.show()
+
+    def poweroff(self):
+        self.write_cmd(SET_DISP | 0x00)
+
+    def contrast(self, contrast):
+        self.write_cmd(SET_CONTRAST)
+        self.write_cmd(contrast)
+
+    def invert(self, invert):
+        self.write_cmd(SET_NORM_INV | (invert & 1))
+
+    def show(self):
+        self.write_cmd(SET_COL_ADDR)
+        self.write_cmd(0)
+        self.write_cmd(self.width - 1)
+        self.write_cmd(SET_PAGE_ADDR)
+        self.write_cmd(0)
+        self.write_cmd(self.pages - 1)
+        self.write_data(self.buffer)
+
+    def fill(self, col):
+        self.framebuf.fill(col)
+
+    def pixel(self, x, y, col):
+        self.framebuf.pixel(x, y, col)
+
+    def scroll(self, dx, dy):
+        self.framebuf.scroll(dx, dy)
+
+    def text(self, string, x, y, col=1):
+        self.framebuf.text(string, x, y, col)
+
+    def line(self, start, end, col=1):
+        # Setup initial conditions
+        x1, y1 = start
+        x2, y2 = end
+        dx = x2 - x1
+        dy = y2 - y1
+     
+        # Determine how steep the line is
+        is_steep = abs(dy) > abs(dx)
+     
+        # Rotate line
+        if is_steep:
+            x1, y1 = y1, x1
+            x2, y2 = y2, x2
+     
+        # Swap start and end points if necessary and store swap state
+        swapped = False
+        if x1 > x2:
+            x1, x2 = x2, x1
+            y1, y2 = y2, y1
+            swapped = True
+     
+        # Recalculate differentials
+        dx = x2 - x1
+        dy = y2 - y1
+     
+        # Calculate error
+        error = int(dx / 2.0)
+        ystep = 1 if y1 < y2 else -1
+     
+        # Iterate over bounding box generating points between start and end
+        y = y1
+        points = []
+        for x in range(x1, x2 + 1):
+            coord = (y, x) if is_steep else (x, y)
+            points.append(coord)
+            error -= abs(dy)
+            if error < 0:
+                y += ystep
+                error += dx
+     
+        # Reverse the list if the coordinates were swapped
+        if swapped:
+            points.reverse()
+
+        for point in points:
+            self.framebuf.pixel(point[0], point[1], col)
+
+    def circle(self, x0, y0, radius):
+        x = radius
+        y = 0
+        err = 0
+
+        while x >= y:
+            self.framebuf.pixel(x0 + x, y0 + y, 1)
+            self.framebuf.pixel(x0 + y, y0 + x, 1)
+            self.framebuf.pixel(x0 - y, y0 + x, 1)
+            self.framebuf.pixel(x0 - x, y0 + y, 1)
+            self.framebuf.pixel(x0 - x, y0 - y, 1)
+            self.framebuf.pixel(x0 - y, y0 - x, 1)
+            self.framebuf.pixel(x0 + y, y0 - x, 1)
+            self.framebuf.pixel(x0 + x, y0 - y, 1)
+
+            y += 1
+            err += 1 + 2*y
+            if 2*(err-x) + 1 > 0:
+                x -= 1
+                err += 1 - 2*x    
+
+class SSD1306_I2C(SSD1306):
+    def __init__(self, height, i2c, addr=0x3c, external_vcc=False):
+        self.i2c = i2c
+        self.addr = addr
+        self.temp = bytearray(2)
+        super().__init__(height, external_vcc)
+
+    def write_cmd(self, cmd):
+        self.temp[0] = 0x80 # Co=1, D/C#=0
+        self.temp[1] = cmd
+        self.i2c.writeto(self.addr, self.temp)
+
+    def write_data(self, buf):
+        self.temp[0] = self.addr << 1
+        self.temp[1] = 0x40 # Co=0, D/C#=1
+        self.i2c.start()
+        self.i2c.write(self.temp)
+        self.i2c.write(buf)
+        self.i2c.stop()
+
+    def poweron(self):
+        pass
